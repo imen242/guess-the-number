4ed9c5e870576c72a2588df13417adaba1b849fb
imen242 <imenemadani380@icloud.com>
2026-06-27 09:22:03 +0100

    Refactor guess input handling in number guessing game

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ secret_number = random.randint(1,100)
 print("welcome to guess the number game!")
 print(" i'm thinking of a number between 1 and 100. can you guess it?")
 while True :
-  guess_str= input("enter you guess: ")
-  guess= int(guess_str)
-if guess> secret_number:
-  print ("your guess is too high. guess again")
-elif guess< secret_number:
-  print(" your guess is too low. guess again")
-else:
-  print("congragulations! you guessed the number correctly!!!!")
-  break
-  
+    guess_str= input("enter you guess: ")
+    guess= int(guess_str)
+    if guess> secret_number:
+        print ("your guess is too high. guess again")
+    elif guess< secret_number:
+        print(" your guess is too low. guess again")
+    else:
+        print("congragulations! you guessed the number correctly!!!!")
+        break
